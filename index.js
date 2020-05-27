024d85358bb92d4bdcff3d939c374d9ab9761cc5
Lorena silva <lorenacunha@outlook.com>
2020-05-27 01:11:49 -0300

    event emitter

--- index.js
+++ index.js
@@ -1,56 +1,18 @@
-const util = require("util");
-const getAddressAsync = util.promisify(getAddress);
-function getUser() {
-  return new Promise(function resolvePromise(resolve, reject) {
-    setTimeout(() => {
-      return resolve({
-        id: 1,
-        name: "Laura",
-        birthday: new Date(),
-      });
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {
 
-      return reject(new Error("ops"));
-    }, 1000);
-  });
 }
 
-function getPhone(id) {
-  return new Promise(function resolvePromise(resolve, reject) {
-    setTimeout(() => {
-      return resolve({
-        phone: "9083240-2342",
-        ddd: 31,
-      });
-    }, 1000);
-  });
-}
-function getAddress(id, callback) {
-  setTimeout(() => {
-    return callback(null, {
-      street: "dumbie's",
-      number: "zero",
-    });
-  }, 1000);
-}
+const myEmitter = new MyEmitter();
+const nameEvent = 'usuario:click';
+myEmitter.on(nameEvent, (click)=>{
+    console.log('user click', click)
+})
 
-main();
-async function main() {
-  try {
-    console.time("medida-promise");
-    const user = await getUser();
-    const result = await Promise.all([getPhone(user.id), getAddressAsync(user.id)]);
-    const phone = result[0];
-    const address = result[1];
-    console.log(`
-            user: ${user.name}
-            birthday: ${user.birth}
-            phone: ${phone.ddd}-${phone.phone}
-            address: 
-                street:${address.street}
-                number:${address.number}
-        `);
-    console.timeEnd("medida-promise");
-  } catch (eror) {
-    console.log(error);
-  }
-}
+myEmitter.emit(nameEvent, 'scroll');
+myEmitter.emit(nameEvent, 'enter');
+
+let count = 0;
+setInterval(()=>{
+    myEmitter.emit(nameEvent, 'space ' + (count++))
+}, 1000)
